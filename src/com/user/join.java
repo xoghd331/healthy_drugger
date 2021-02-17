418e33fd473907f826fb909cd23b6c04a2944f8b
Chosun <Chosun@DESKTOP-LFSHGNF>
2021-02-17 15:08:19 +0900

    join 컨트롤러 수정했습니다.

--- src/com/user/join.java
+++ src/com/user/join.java
@@ -25,6 +25,11 @@ public class join extends HttpServlet {
 		String tel = request.getParameter("tel");
 		String license = request.getParameter("license");
 
+		System.out.println("[Join] email : "+id);
+		System.out.println("[Join] pw : "+pw);
+		System.out.println("[Join] tel : "+tel);
+		System.out.println("[Join] addr : "+license);
+		
 		UserDAO dao = new UserDAO();
 		UserDTO dto = new UserDTO(id, pw, tel, license);
 
@@ -33,10 +38,11 @@ public class join extends HttpServlet {
 		if (cnt > 0) {
 			HttpSession session = request.getSession();
 			session.setAttribute("id", id);
-			
-			response.sendRedirect(/* "로그인 성공 페이지" */);
+			System.out.println("회원가입 성공!");
+			response.sendRedirect("login-join/login.jsp");
 		}else {
-			response.sendRedirect(/* "main페이지" */);
+			System.out.println("회원가입 실패!");
+			response.sendRedirect("login-join/join.jsp");
 		}
 	}
 
