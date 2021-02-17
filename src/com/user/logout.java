16d769f64022d386ce5f451244f5e86ad56e55a7
Chosun <Chosun@DESKTOP-LFSHGNF>
2021-02-17 16:14:44 +0900

    logout 서블릿 연결했어요

--- src/com/user/logout.java
+++ src/com/user/logout.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+@WebServlet("/logout")
 public class logout extends HttpServlet {
 
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
